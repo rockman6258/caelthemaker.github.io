1f00a78d081937c8dd769d5db06343dafbf6236c
rockman6258 <64748238+rockman6258@users.noreply.github.com>
2022-10-19 21:00:27 -0500

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-/* You're going to have to move this to script tags to make it work on google sites. */
-
-const studentAdder = document.getElementById("studentAdder");
-
-studentAdder.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    console.log("Form submitted");
-});
